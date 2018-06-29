2567ed63673b22b8fbc2b8f1bc7c4eec2b15c50c
DalThePal <dallen.hoyal@gmail.com>
2018-06-28 19:58:43 -0600

    fixed home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,19 +22,18 @@ export default class Home extends Component {
                 <SubHeader />
                 <div className='body'>
                     <div className='img-button'>
-                        <img src='/dummy-img1.jpg' alt='how we started'/>
-                        <span>How we started...</span>
+                        <img src='/dummy-img1.jpg' alt='how we started' width='100%'/>
+                        <div>How we started...</div>
                     </div>
                     <div className='img-button'>
-                        <img/>
-                        <span>What inspires us...</span>
+                        <img src='/dummy-img1.jpg' alt='what inspires us' width='100%'/>
+                        <div>What inspires us...</div>
                     </div>
                     <div className='img-button'>
-                        <img/>
-                        <span>Employee favorites...</span>
+                        <img src='/dummy-img1.jpg' alt='what inspires us' width='100%'/>    
+                        <div>Employee favorites...</div>
                     </div>
                 </div>
-                <Footer />
             </div>
         )
     }
